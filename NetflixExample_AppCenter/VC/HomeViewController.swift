15fc476dc761bb14742d72e0dc39aef2a428e2b4
Leemhyungyu <lhglbkksj@naver.com>
2022-05-09 16:32:47 +0900

    HomeVC 영화 목록 화면 구현 (1)

--- NetflixExample_AppCenter/VC/HomeViewController.swift
+++ NetflixExample_AppCenter/VC/HomeViewController.swift
@@ -6,9 +6,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class HomeViewController: UIViewController {
 
+    var awardRecommendListViewController: RecommendListViewController!
+    var hotRecommendListViewController: RecommendListViewController!
+    var myRecommendListViewController: RecommendListViewController!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "award" {
+            let destinationVC = segue.destination as? RecommendListViewController
+            awardRecommendListViewController = destinationVC
+            awardRecommendListViewController.viewModel.updateType(.award)
+            awardRecommendListViewController.viewModel.fetchItems()
+        } else if segue.identifier == "hot" {
+            let destinationVC = segue.destination as? RecommendListViewController
+            hotRecommendListViewController = destinationVC
+            hotRecommendListViewController.viewModel.updateType(.hot)
+            hotRecommendListViewController.viewModel.fetchItems()
+        } else if segue.identifier == "my" {
+            let destinationVC = segue.destination as? RecommendListViewController
+            myRecommendListViewController = destinationVC
+            myRecommendListViewController.viewModel.updateType(.my)
+            myRecommendListViewController.viewModel.fetchItems()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
