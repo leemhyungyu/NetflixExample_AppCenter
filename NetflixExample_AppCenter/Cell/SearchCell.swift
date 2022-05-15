4ccb725c5cab03180327cd20275ea5202b1d5f9a
Leemhyungyu <lhglbkksj@naver.com>
2022-05-15 15:44:22 +0900

    검색화면 구현 (SearchVC에 SearchCell연결)

---
+++ NetflixExample_AppCenter/Cell/SearchCell.swift
@@ -0,0 +1,36 @@
+//
+//  SearchCell.swift
+//  NetflixExample_AppCenter
+//
+//  Created by 임현규 on 2022/05/15.
+//
+
+import UIKit
+
+class SearchCell: UICollectionViewCell {
+
+    static let identifier = "SearchCell"
+    
+    var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "img_movie_1")
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+
+    override func layoutSubviews() {
+        setUp()
+    }
+    private func setUp() {
+        addSubview(imageView)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+//        imageView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+//        imageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+//        imageView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+//        imageView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 160).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 120).isActive = true
+    }
+}
+
--- NetflixExample_AppCenter/VC/SearchViewController.swift
+++ NetflixExample_AppCenter/VC/SearchViewController.swift
@@ -21,9 +21,10 @@ class SearchViewController: UIViewController {
     var collectionView: UICollectionView = {
     
         let layout = UICollectionViewFlowLayout()
-        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        
-        return cv
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(SearchCell.self, forCellWithReuseIdentifier: SearchCell.identifier)
+
+        return collectionView
     }()
     
     
@@ -35,12 +36,41 @@ class SearchViewController: UIViewController {
     
     func setUp() {
         view.addSubview(searchBar)
+        view.addSubview(collectionView)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        collectionView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        collectionView.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 10).isActive = true
+        
         searchBar.delegate = self
         searchBar.translatesAutoresizingMaskIntoConstraints = false
         searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
         searchBar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
         searchBar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
     }
+}
+
+extension SearchViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        guard let searchCell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchCell.identifier, for: indexPath) as? SearchCell else { return UICollectionViewCell() }
+        
+        return searchCell
+                
+    }
+    
+    
 }
 
 extension SearchViewController: UISearchBarDelegate {
